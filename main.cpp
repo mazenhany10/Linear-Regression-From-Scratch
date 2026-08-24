d566287fbaf44df0c47dadf402606124184996d7
Mazen <mazenhanyismail@gmail.com>
2026-08-24 23:08:11 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 using namespace std;
- int main (){
-      int favorite_number;
-     cout<< "Enter your favorite number  between 1 and 100"<<endl ;
-     
-     cin >> favorite_number;
-     
-     cout << "Amazing!!!!!! that's my favorite number too"<<endl;
-      
-      return 0;
-      
- }
+int main() {
+    cout << "Frank's Carpet Cleaning Service"<< endl;
+    cout<< " how many small rooms";
+    int small_rooms{0};
+    cin >> small_rooms;
+    cout<< " how many large rooms";
+    int large_rooms{0};
+    cin >> large_rooms;
+    const double price_of_small_room{15};
+    const double price_of_large_room{40};
+    const double tax_rate{0.06};
+    const int estimate_days{30};
+    // Calculate costs
+    const double cost_small = price_of_small_room * small_rooms;
+    const double cost_large = price_of_large_room * large_rooms;
+    const double subtotal = cost_small + cost_large;
+    const double tax = subtotal * tax_rate;
+    const double total = subtotal + tax;
+
+    cout << "Price per small room: $" << price_of_small_room << endl;
+    cout << "Price per large room: $" << price_of_large_room << endl;
+    cout << "Cost: $" << subtotal << endl;
+    cout << "Tax: $" << tax << endl;
+    cout << "====================================" << endl;
+    cout << "Total estimate: $" << total << endl;
+    cout << "This estimate is valid for " << estimate_days << " days" << endl;
+
+    return 0;
+}
