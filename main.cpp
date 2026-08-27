62f84594e7fd8010ffd912f45597ac1ecf243ee0
Mazen <mazenhanyismail@gmail.com>
2026-08-28 02:57:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
+//
+//  main.cpp
+//  multply
+//
+//  Created by Mazenn  on 28/08/2026.
+//
+
 #include <iostream>
 using namespace std;
-int main(){
-    const int pound{100};
-    const int half_pound{50};
-    const int  Piaster {10};
-    int amount {};
-    cout << "put the number in Piaster ";
-    cin >> amount;
-    int balance{},pounds{}, half_pounds{},Piasters{};
-    pounds =amount/pound;
-    balance=amount%pound;
+int main() {
+    // insert code here...
+    for (int i =1; i<=13; i++) {
+        for (int j=1; j<=13; j++) {
+            cout << i<<'*'<<j<<'='<<i*j<<endl;
+        }cout <<"========"<<endl;
     
-    half_pounds=balance/half_pound;
-    balance%=half_pound;
+    }
     
-    Piasters=balance/Piaster;
-    balance%=Piaster;
-    cout <<pounds<<endl;
-    cout <<half_pounds<<endl;
-    cout <<Piasters<<endl;
-
+    
+    
+    
 }
-
-
-
