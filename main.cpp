8b7e729765ccf6cb4895e41aaa638b28ecd42c8c
Mazen <mazenhanyismail@gmail.com>
2026-09-03 21:22:09 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,42 @@
-//
-//  main.cpp
-//  Letter Pyramid
-//
-//  Created by Mazenn  on 29/08/2026.
-//
-
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 int main() {
-    string letters  {};
-    cin >>letters;
+    int score (100);
+    int *score_ptr {&score};
+    cout << &score_ptr << endl; // 100
+    cout << *score_ptr << endl;
+    *score_ptr = 200;
+    cout << *score_ptr << endl; // 200
+    cout << score << endl;// 200
     
-    for (int i =0; i < letters.length(); i++) {
-        for (int space =0; space < letters.length()-1-i ; space ++) {
-            cout << " ";
-        }cout << letters.substr(0, i+1);
-        for (int j = i - 1; j >= 0; j--) {
-                   cout << letters.at(j);
-               }
-               
-               cout << endl;
-           }
-           
-           return 0;
-       }
+    
+    
+    double high_temp{100.89};
+    double low_temp{10.89};
+    double *temp_ptr{&low_temp};
+    cout<<*temp_ptr<<endl;
+    temp_ptr=&high_temp;
+    cout << *temp_ptr<<endl;
+    cout << temp_ptr<<endl;
+    
+    string name{"frank"};
+    string *string_ptr{&name};
+    cout << *string_ptr<<endl;
+    cout << string_ptr<<endl;
+    
+    
+    vector<string>stooges {"Mazen","Nada","Malak"};
+    vector<string>*vector_ptr{nullptr};
+    vector_ptr=&stooges;
+    cout<<"first: "<< (*vector_ptr).at(0)<<endl;
+    cout << "stooges"<< endl;
+    for (auto stooges: *vector_ptr) {
+        cout << stooges << " "<<endl;
+    }
+    
+    
+    
+    
+}
