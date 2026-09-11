63f0a95364376b8c8da2b8c7a27a462882d8b2b2
Mazen <mazenhanyismail@gmail.com>
2026-09-11 18:04:32 +0300

    Add files via upload

--- Account.hpp
+++ Account.hpp
@@ -1,19 +1,39 @@
 #ifndef Account_hpp
 #define Account_hpp
 #include <string>
+#include <iostream>
 using namespace std;
-class Account {
+class Player{
 private:
-    string name;
-    double balance;
+    string name ;
+    int health;
+    int xp;
 public:
-    void set_balance(double bal){balance=bal;}
-    double get_balance(){return balance ;}
-    void set_name(string n);
-    string get_name();
-    bool deposit(double amount);
-    bool withdraw(double amount);
+    void set_name(string n){
+        name=n;
+        
+    }
+//constructors
+    Player(){
+        cout<< "no args constructors are called"<< endl;
+        
+    }
+    Player(string name){
+        this->name = name;
+           cout << "string constructor called" << endl;
+       
+    
+    }
+    Player (string name , int health,int xp){
+        this->name = name;
+           this->health = health;
+           this->xp = xp;
+           cout << "three args constructor called" << endl;
+    
+    }
+    ~Player(){
+        cout<< "destractor called for "<< name << endl;
+    }
     
 };
-
 #endif // !Account_hpp
--- main.cpp
+++ main.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 #include "Account.hpp"
 int main(){
-    Account mazen_account;
-    mazen_account.set_name("Mazen's account");
-    mazen_account.set_balance(100000);
-    if (mazen_account.deposit(900))
-        cout<<"ok deposit"<<endl;
-    else
-        cout <<"deposit is not allowed"<<endl;
-    
-    if (mazen_account.withdraw(300))
-        cout<<"ok withdaw"<<endl;
-    else
-        cout<<"not sufficient funds"<<endl;
-    
-    if (mazen_account.withdraw(300000))
-        cout<<"ok withdaw"<<endl;
-    else
-        cout<<"not sufficient funds"<<endl;
-    cout << "Account name: " << mazen_account.get_name() << endl;
-    cout << "Balance: " << mazen_account.get_balance() << endl;
+    {
+        Player slayer;
+        slayer.set_name("salayer");
+    }
+    {
+        Player mazen;
+        mazen.set_name("mazen");
+        Player mustafa ("mustafa");
+        mustafa.set_name("mustafa");
+        Player lollipop("lollipop",100,86);
+        lollipop.set_name("lollipop");
+    }
+    Player*enemey= new Player;
+    enemey->set_name("enemYY");
+    Player *levelboss= new Player("leveboss",900,70);
+    levelboss->set_name("leveboss");
+    delete enemey;
+    delete levelboss;
     
 }
