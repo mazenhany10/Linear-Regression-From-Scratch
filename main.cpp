0ac8eb2fd172b2b7bb60d29a3b64eedb41a631ae
Mazen <mazenhanyismail@gmail.com>
2026-09-13 21:46:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,241 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+int kbhit()
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
+}
+
+int getch()
+{
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
 using namespace std;
-#include "Account.hpp"
-int main(){
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitx, fruity, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection
+{
+    stop = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+eDirection dir;
+void set_up()
+{
+    gameOver = false;
+    dir = stop;
+    x = width / 2;
+    y = height / 2;
+    fruitx = rand() % width;
+    fruity = rand() % height;
+    score = 0;
+    nTail = 0;
+}
+void draw()
+{
+    system("clear");
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
     {
-        Player slayer;
-        slayer.set_name("salayer");
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruity && j == fruitx)
+                cout << "F";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "*";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
     }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score:" << score << endl;
+}
+// void input()
+// {
+//     while (kbhit())
+//     {
+//         switch (getch())
+//         {
+//         case 'a':
+//             dir = LEFT;
+//             break;
+//         case 'd':./snake
+//             dir = RIGHT;
+//             break;
+//         case 'w':
+//             dir = UP;
+//             break;
+//         case 's':
+//             dir = DOWN;
+//             break;
+//         case 'p':
+//             gameOver = true;
+//             break;
+//         }
+//     }
+// }
+void input()
+{
+    while (kbhit())
     {
-        Player mazen;
-        mazen.set_name("mazen");
-        Player mustafa ("mustafa");
-        mustafa.set_name("mustafa");
-        Player lollipop("lollipop",100,86);
-        lollipop.set_name("lollipop");
+        switch (getch())
+        {
+        case 'a':
+            if (dir != RIGHT)
+                dir = LEFT;
+            break;
+        case 'd':
+            if (dir != LEFT)
+                dir = RIGHT;
+            break;
+        case 'w':
+            if (dir != DOWN)
+                dir = UP;
+            break;
+        case 's':
+            if (dir != UP)
+                dir = DOWN;
+            break;
+        case 'p':
+            gameOver = true;
+            break;
+        }
     }
-    Player*enemey= new Player;
-    enemey->set_name("enemYY");
-    Player *levelboss= new Player("leveboss",900,70);
-    levelboss->set_name("leveboss");
-    delete enemey;
-    delete levelboss;
-    
+}
+void logic()
+{
+    int prevx = tailX[0];
+    int prevy = tailY[0];
+    int prev2x, prev2y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2x = tailX[i];
+        prev2y = tailY[i];
+        tailX[i] = prevx;
+        tailY[i] = prevy;
+        prevx = prev2x;
+        prevy = prev2y;
+    }
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+
+    default:
+        break;
+    }
+    // if (x >= width || x < 0 || y >= height || y < 0)
+    //  gameOver = true;
+
+    if (x >= width)
+        x = 0;
+    else if (x < 0)
+        x = width - 1;
+    if (y >= height)
+        y = 0;
+    else if (y < 0)
+        y = height - 1;
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+    if (x == fruitx && y == fruity)
+    {
+        score += 10;
+        fruitx = rand() % width;
+        fruity = rand() % height;
+        nTail++;
+    }
+}
+
+int main()
+{
+    srand(time(0));
+    set_up();
+    while (!gameOver)
+    {
+        draw();
+        input();
+        logic();
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    }
+    return 0;
 }
