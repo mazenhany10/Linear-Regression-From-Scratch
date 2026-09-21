ec92c6a00be701857588e73f83d73cfd1e9a8e34
Mazen <mazenhanyismail@gmail.com>
2026-09-21 03:58:03 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,21 +7,26 @@ private:
     int health ;
     int xp;
 public:
-    void set_name(string name_val){
-        name = name_val;
-    }
-    string get_name(){
-        return name;
-    }
-    Player(){
-        name= "none";
-        health =100;
-        xp=3;
-    }
+    Player();
+    Player(string name_val);
+    Player(string name_val,int health_val,int xp_val);
+  
 };
+Player::Player()
+:Player{"None",0,0}{
+    cout << " no args contrutor"<<endl;
+}
+Player::Player(string name_val)
+:Player{name_val,0,0}{
+    cout << " one contrutor"<<endl;
+}
+Player::Player(string name_val,int health_val,int xp_val)
+:name{name_val},health{health_val},xp{xp_val}{
+    cout << " all contrutor has been called "<<endl;
+}
+
 int main() {
-    Player mazen ;
-    mazen.set_name("Mazen");
-    cout << mazen.get_name()<<endl;
-    
+    Player empty ;
+    Player frank{"frank"};
+    Player mazen {"mazen",100,90};
 }
