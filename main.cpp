531892ebd8898e0fb497bfa1c379c342dfd9ab80
Mazen <mazenhanyismail@gmail.com>
2026-09-22 18:15:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 using namespace std;
-class Player
+class Shallow
 {
 private:
-    string name ;
-    int health ;
-    int xp;
-public:
-
-    string get_name(){return name;}
-    int get_health(){return health;}
-    int get_xp(){return xp;}
-    Player(string name_val="None",int health_val=0,int xp_val=0);
-  //copy
-    Player(const Player & source );
-    //destractor
-    ~Player(){cout <<"dustructor is called for"<< name<<endl;}
-};
-Player::Player(string name_val,int health_val,int xp_val)
-:name{name_val},health{health_val},xp{xp_val}{
-    cout << "All contrutor has been called "+ name<<endl;
-}
-Player::Player(const Player & source )
-:name{source.name},health{source.health},xp{source.xp}{
+    int *data ;
     
-    cout << " copy is made from "<< source.name<<endl;
+public:
+    void set_data_value(int d){*data=d;}
+    int get_data_value(){return *data;}
+    // constructor
+    Shallow (int d);
+    //copy instructor
+    Shallow(const Shallow&source);
+    // destructor
+    ~Shallow();
+};
+Shallow::Shallow(int d){
+    data = new int;
+    *data =d;
 }
-void display_player(Player P){
-    cout << "Name: "<< P.get_name()<<endl;
-    cout << "Health : "<< P.get_health()<<endl;
-    cout << "xp: "<< P.get_xp()<<endl;
+Shallow::Shallow(const Shallow & source)
+: data ( source.data ){
+    cout << "copy constructor "<< endl ;
 }
+Shallow::~Shallow(){
+    delete data ;
+    cout <<"Denstructor is freeing data"<< endl;
+}
+void display_shallow (Shallow s){
+    cout << s.get_data_value()<<endl;
+}
+
 int main() {
-    Player empty{" xxxxx",100,50};
-    Player my_new_object{empty};
-    display_player(empty);
-    Player frank{"frank"};
-    Player mustafa{" mustafa",100};
-    Player mazen {" mazen",100,90};
+    Shallow obj1{100};
+    display_shallow(obj1);
+    Shallow obj2 {obj1};
+    obj2.set_data_value(1000);
+    
 }
