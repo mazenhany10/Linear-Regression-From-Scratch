2ac6ef1bbd2e4d6e88751652a3630760bee40b23
Mazen <mazenhanyismail@gmail.com>
2026-09-23 05:32:45 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Move
+class Player
 {
 private:
-    int *data;
+    string name;
+    int health;
+    int xp;
 public:
-    void set_data_value(int d){*data=d;}
-    int get_data_value(){return *data;}
-    Move (int d);
-    Move(const Move&source );
-    Move(Move&& source) noexcept;
-    ~Move();
-    
-};
-Move::Move(int d ){
-    data = new int;
-    *data =d ;
-    cout << "constructor for "<< d <<endl;
-};
-
-
-Move::Move(const Move &source )
-:Move{*source.data}{
-    cout <<"copy move constructor "<< *data << endl ;
-    
-}
-//move con
-Move::Move(Move &&source) noexcept
-:data{source.data}{
-    source.data=nullptr;
-    cout<< "move constructor - remove the source "<<endl;
-}
-
-Move::~Move()
-{
-    if (data !=nullptr){
-        cout << "destructor is freeing data "<< *data<<endl;
-    }else {
-        cout << "Destructor is freing data for null ptr"<< endl;
+   // newwwww
+    string get_name () const{
+        return name;
     }
-    delete data;
-    cout << "delete"<< endl;
+    void set_name(string name_val){
+        name=name_val;
+    }
+    Player();
+    Player(string name_val);
+    Player(string name_val,int health_val,int xp_val);
+};
+Player::Player()
+:Player{"None",0,0}{
+    
+}
+Player::Player(string name_val)
+:Player{name_val,0,0}{
+    
 }
 
+Player::Player(string name_val,int health_val,int xp_val)
+:name{name_val},health{health_val},xp{xp_val}{
+    
+}
+// newwwww
+void display_player(const Player &p){
+    cout << p.get_name()<<endl;
+}
 
 int main() {
-    vector<Move>vec;
-    vec.push_back(Move{10});
-    vec.push_back(Move{20});
-    vec.push_back(Move{30});
-    vec.push_back(Move{40});
-    vec.push_back(Move{50});
-    vec.push_back(Move{60});
-    vec.push_back(Move{70});
-    vec.push_back(Move{80});
+    const Player vallian {"moustafa",100,55};
+    Player hero {"hero",100,15};
+    cout << hero.get_name()<< endl;
+    cout << vallian.get_name()<< endl;
 }
